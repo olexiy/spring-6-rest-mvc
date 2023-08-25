a94d862c08346eb5f33d2708bffadbfc91da4a94
Olexiy Sokurenko <sokurenko-extern@gvl.de>
2023-08-25 12:20:23 +0200

    Added email field to Customer entity and managed schema with Flyway
    Added an "email" field to the Customer entity and created corresponding database migration scripts. Flyway has been integrated for future database version management. This will make tracking and applying schema changes easier and more controlled. Also, changed the configuration to validate the schema on startup instead of updating it.

--- src/main/java/de/olexiy/spring6restmvc/entities/Customer.java
+++ src/main/java/de/olexiy/spring6restmvc/entities/Customer.java
@@ -24,6 +24,8 @@ public class Customer {
     private UUID id;
     private String name;
 
+    private String email;
+
     @Version
     private Integer version;
     private LocalDateTime createdDate;
