c6e0f477f018f72676353e8a19d7b21dd82b516f
Olexiy Sokurenko <sokurenko-extern@gvl.de>
2023-09-29 23:08:47 +0200

    Switch from HttpBasic to OAuth2 resource server
    Transitioned the application's security configuration from using basic Http authentication to relying on an OAuth2 JWT resource server. This change was made to increase the security of the application, as well as to align with industry best practices. Removed Basic Security user credentials from application properties file and added a dependency for OAuth2 server in pom.xml. Disabled the CSRF protection for API endpoints as it's unnecessary with JWT based authentication.

--- src/main/java/de/olexiy/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/de/olexiy/spring6restmvc/config/SpringSecConfig.java
@@ -5,20 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SpringSecConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated()).httpBasic(
-                Customizer.withDefaults()
-        );
-
-        http.csrf(csrf -> csrf.ignoringRequestMatchers(
-                new AntPathRequestMatcher("/api/**")));
-
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 
